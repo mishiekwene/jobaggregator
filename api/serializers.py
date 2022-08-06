38aae5b2a774550bce27e5929f48b213b9d96655
mishiekwene <m.ishiekwene@rgu.ac.uk>
2022-08-06 13:27:57 +0100

    Changes to the seriealizer

--- api/serializers.py
+++ api/serializers.py
@@ -12,5 +12,5 @@ class JobSerializer(serializers.ModelSerializer):
         defaults={"company_name":validated_data.get('company_name', 'null'), "company_link":validated_data.get('company_link', 'null'),"location":validated_data.get('location', 'null'),"num_of_applicants":validated_data.get('num_of_applicants', 'null'),
         "remote":validated_data.get('remote', 0), "salary":validated_data.get('salary', 'null'), "time_extracted":validated_data.get('time_extracted', 'null'), "time_posted":validated_data.get('time_posted', 'null'), "job_type":validated_data.get('job_type', 'null'),
         'job_type1':validated_data.get('job_type1', 'null'), 'job_type2':validated_data.get('job_type2', 'null'), 'lower_sal':validated_data.get('lower_sal', 'null'), 'higher_sal':validated_data.get('higher_sal', 'null'), 'avg_sal':validated_data.get('avg_sal', 'null'),
-        'sal_freq':validated_data.get('sal_freq', 'null'), 'state':validated_data.get('state', 'null'), 'county':validated_data.get('county', 'null'), 'date_posted':validated_data.get('date_posted', 'null')})
+        'sal_freq':validated_data.get('sal_freq', 'null'), 'state':validated_data.get('state', 'null'), 'county':validated_data.get('county', 'null'), 'date_posted':validated_data.get('date_posted', 'null'), 'city': validated_data.get("city", "null")})
         return job
